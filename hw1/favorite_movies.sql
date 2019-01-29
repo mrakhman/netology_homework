de8b925b2dc3322812ac9ea49254a6de4c84df18
Mariya Rakhman <robinbad1312@gmail.com>
2019-01-29 13:25:20 +0000

    finish

--- hw1/favorite_movies.sql
+++ hw1/favorite_movies.sql
@@ -1,3 +1,8 @@
+DROP TABLE IF EXISTS films;
+DROP TABLE IF EXISTS persons;
+DROP TABLE IF EXISTS persons2content;
+
+
 CREATE TABLE films (
         id SERIAL PRIMARY KEY,
         title VARCHAR (256) NOT NULL,
@@ -5,10 +10,12 @@ CREATE TABLE films (
         box_office INTEGER NOT NULL,
         release_year TIMESTAMP
 );
+
 CREATE TABLE persons (
         id SERIAL PRIMARY KEY,
         fio VARCHAR (64) NOT NULL
 );
+
 CREATE TABLE persons2content (
         person_id INTEGER REFERENCES persons(id),
         film_id INTEGER REFERENCES films(id),
@@ -25,12 +32,31 @@ VALUES
 
 INSERT INTO persons(fio)
 VALUES 
+        ('Marc Webb'),
+        ('Zooey Deschanel'),
+        ('Joseph Gordon-Levitt'),
         ('Matthew McConaughey'),
         ('Anne Hathaway'),
         ('Chiwetel Ejiofor'),
         ('Lupita Nyongo'),
         ('Saoirse Ronan'),
         ('Jean Dujardin'),
-        ('Bérénice Bejo');
+        ('Bérénice Bejo'),
+        ('Christopher Nolan'),
+        ('John Crowley');
 
-INSERT INTO persons2content(fio)
+INSERT INTO persons2content(person_type)
+VALUES
+        (1, 1, 'director'),
+        (2, 1, 'actor'),
+        (3, 1, 'actor'),
+        (4, 3, 'actor'),
+        (5, 3, 'actor'),
+        (6, 5, 'actor'),
+        (7, 5, 'actor'),
+        (8, 2, 'actor'),
+        (9, 4, 'actor'),
+        (10, 4, 'actor'),
+        (11, 3, 'director'),
+        (12, 2, 'director');
+
