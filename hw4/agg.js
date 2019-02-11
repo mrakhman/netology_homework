daf1711fcde43696d3114f7c0950aa3653a2f1e1
Mariya RAKHMAN <mrakhman@student.42.fr>
2019-02-11 19:17:53 +0100

    almost done

--- hw4/agg.js
+++ hw4/agg.js
@@ -1,24 +1,30 @@
 /*
         Написать запрос, который выводит общее число тегов
 */
-print("tags count: ", 'расчёт количества тегов');
+//print(tags count: , 'db.tags.count()'); // OR: db.tags.aggregate([{$group: {_id: total, tag_count: {$sum: 1}}}])
 /*
         Добавляем фильтрацию: считаем только количество тегов woman
 */
-print("woman tags count: ", 'расчёт количества тегов woman');
+//print(woman tags count: , 'db.tags.count({name: woman})'); // OR: 
 /*
         Очень сложный запрос: используем группировку данных посчитать количество вхождений для каждого тега
         и напечатать top-3 самых популярных
 */
 
+
+// Full request: db.tags.aggregate([{$group: {_id: $name, tag_count: {$sum: 1}}}, {$sort: {tag_count: -1}}, {$limit: 3}])
+
 printjson(
         db.tags.aggregate([
-                {"$group": {
-                                "--": "--",
-                                "--": "--"
+                {$group: {
+                                id: $name,
+                                tag_count: {$sum: 1}
                            }
                 },
-                {"тут модификатор sort по полю с аггрегацией"},
+                {$sort: {tag_count: -1}},
                 {$limit: 3}
         ])['_batch']
 );
+
+
+
