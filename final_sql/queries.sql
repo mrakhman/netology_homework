586438f3aa174730e13c1ab6c7747cb823669a7e
Mariya RAKHMAN <mrakhman@student.42.fr>
2019-02-25 22:26:01 +0100

    queries

--- final_sql/queries.sql
+++ final_sql/queries.sql
@@ -1,10 +1,4 @@
--- W 1 Показать топ 10 клиентов, которые потратили больше всего денег в антикафе [expense]
-
--- W 2 Показать число посещений и выручку за ноябрь 2018 по каждому антикафе [expense + visit]
-
--- W 3 Показать, какую выручку приносят клиенты с каждым типом карты (card_level) [expense + client]
-
-
+-- 3 Показать, какую выручку приносят клиенты с каждым типом карты (card_level) [expense + client]
 
 -- 4 Показать долю посещений клиентов с гостевой картой от общего числа посещений по каждому антикафе [client + visit]
 
@@ -12,14 +6,57 @@
 
 -- 6 Показать среднее количество посещений за последние 3 месяца в выходной день и в рабочий [visit]
 
-
--- 8 Топ 10 количества новых клиентов за смену [client + shift]
+-- 8 Топ 10 количества регистраций новых клиентов за смену [client + shift]
 
 -- 9 Среднее количество гостей за утреннюю, дневную, вечернюю смену [visit + shift]
 
--- 10 Выбрать всех клиентов, которых зарегали первые 3 человека и посмотреть, сколько количество их посещений за время выборки
 
 SET search_path TO final_sql;
+
+-- 1 [expense]
+-- Показать топ 10 клиентов, которые потратили больше всего денег в антикафе.
+SELECT client_id, sum(amount) AS total_expense
+FROM expense
+GROUP BY client_id
+ORDER BY sum(amount) DESC
+LIMIT 10;
+
+
+-- 2 [expense + visit]
+-- Показать число посещений и выручку за ноябрь 2018 по каждому антикафе.
+WITH n_visits AS
+(
+	SELECT visit.place_id, count(visit_id) AS n_visits
+	FROM visit
+	WHERE start_utc >= '2018-10-01 00:00:00'
+	AND start_utc < '2018-11-01 00:00:00'
+	GROUP BY visit.place_id
+)
+
+SELECT expense.place_id, sum(amount) AS revenue, n_visits
+FROM expense LEFT JOIN n_visits
+ON expense.place_id = n_visits.place_id
+WHERE expense.creation_utc >= '2018-10-01 00:00:00'
+AND expense.creation_utc < '2018-11-01 00:00:00'
+GROUP BY expense.place_id, n_visits.n_visits
+ORDER BY sum(amount) DESC;
+
+-- 3 [expense + client]
+-- Показать, какую выручку в % приносят клиенты с каждым типом карты (card_level)
+SELECT card_level, sum(amount) AS revenue,
+sum(amount) OVER () AS total_rev
+FROM client INNER JOIN expense
+ON client.client_id = expense.client_id; --  2850820-wrong total_rev = 8316332
+;
+ ЗАСТРЯЛА ТУТ
+
+SELECT card_level, sum(amount) AS revenue
+FROM client INNER JOIN expense
+ON client.client_id = expense.client_id
+GROUP BY card_level
+ORDER BY sum(amount) DESC;
+
+
 
 -- 7 [client]
 -- Кто из сотрудников зарегистрировал больше всего клиентов, топ 10.
@@ -28,6 +65,7 @@ FROM client
 GROUP BY creation_place_id, creator_employee_id
 ORDER BY count(creator_employee_id) DESC
 LIMIT 10;
+
 
 -- 10 [client + visit]
 -- Выбрать всех клиентов, которых зарегали первые 3 сотрудника (из предыдущего запроса). 
@@ -54,4 +92,6 @@ ON visit.client_id = client_list.client_id
 GROUP BY creator_employee_id, visit.client_id
 ORDER BY count(visit_id) DESC
 LIMIT 40;
+
+
 
