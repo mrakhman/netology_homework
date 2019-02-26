277d29ce6f2326258b6a7efef11f1b82b0a1bb4f
Mariya RAKHMAN <mrakhman@student.42.fr>
2019-02-26 22:13:34 +0100

    finished

--- final_sql/queries.sql
+++ final_sql/queries.sql
@@ -1,13 +1,3 @@
-
--- 5 Показать, клиенты с каким уровнем карты чаще всего бронируют комнаты [booking + client]
-
--- 6 Показать среднее количество посещений за последние 3 месяца в выходной день и в рабочий [visit]
-
--- 8 Топ 10 количества регистраций новых клиентов за смену [client + shift]
-
--- 9 Среднее количество гостей за утреннюю, дневную, вечернюю смену [visit + shift]
-
-
 SET search_path TO final_sql;
 
 -- 1 [expense]
@@ -51,17 +41,7 @@ GROUP BY card_level
 ORDER BY sum(amount) DESC;
 
 
--- 4 [client + visit]
--- Показать долю посещений клиентов с гостевой картой от общего числа посещений отдельно по каждому клубу
-SELECT place_id, count(visit_id) AS total_visits, 
-count(visit_id) FILTER (WHERE client_id IN (SELECT client_id FROM client WHERE card_level = 1)) AS guest_visits,
-100 * count(visit_id) FILTER (WHERE client_id IN (SELECT client_id FROM client WHERE card_level = 1)) / count(visit_id) AS guests_percent
-FROM visit
-GROUP BY visit.place_id
-ORDER BY count(visit_id) DESC;
-
-
--- 7 [client]
+-- 4 [client]
 -- Кто из сотрудников зарегистрировал больше всего клиентов, топ 10.
 SELECT creation_place_id AS cafe_id, creator_employee_id AS employee_id, count(creator_employee_id) AS n_registered_users
 FROM client
@@ -70,7 +50,7 @@ ORDER BY count(creator_employee_id) DESC
 LIMIT 10;
 
 
--- 10 [client + visit]
+-- 5 [client + visit]
 -- Выбрать всех клиентов, которых зарегали первые 3 сотрудника (из предыдущего запроса). 
 -- Посмотреть количество посещений по каждому сотруднику отдельно у топ 40 клиентов.
 WITH first_three AS
@@ -95,6 +75,68 @@ ON visit.client_id = client_list.client_id
 GROUP BY creator_employee_id, visit.client_id
 ORDER BY count(visit_id) DESC
 LIMIT 40;
+
+
+-- 6 [client + visit]
+-- Показать долю посещений клиентов с гостевой картой от общего числа посещений по каждому антикафе.
+SELECT place_id, count(visit_id) AS total_visits, 
+count(visit_id) FILTER (WHERE client_id IN (SELECT client_id FROM client WHERE card_level = 1)) AS guest_visits,
+100 * count(visit_id) FILTER (WHERE client_id IN (SELECT client_id FROM client WHERE card_level = 1)) / count(visit_id) AS guests_percent
+FROM visit
+GROUP BY visit.place_id
+ORDER BY count(visit_id) DESC;
+
+
+-- 7 [booking + client]
+-- Показать, клиенты с каким уровнем карты чаще всего бронируют комнаты, по убыванию.
+SELECT card_level, count(booking_id) AS n_bookings
+FROM booking INNER JOIN client
+ON booking.client_id = client.client_id
+GROUP BY card_level
+ORDER BY count(booking_id) DESC;
+
+
+-- 8 [client + shift]
+-- Топ 10 количества регистраций новых клиентов за смену.
+SELECT shift_id, shift.employee_id, count(client.client_id)
+FROM shift INNER JOIN client
+ON shift.employee_id = client.creator_employee_id
+WHERE client.creation_utc >= shift.start_utc
+AND client.creation_utc <= shift.finish_utc
+GROUP BY shift.shift_id, shift.employee_id
+ORDER BY count(client.client_id) DESC
+LIMIT 10;
+
+
+-- 9 [visit]
+-- Показать среднее количество посещений за последние 3 месяца (23.08.18 - 23.11.18) в выходной день и в рабочий.
+SELECT 
+count(visit_id) FILTER (WHERE EXTRACT(DOW FROM start_utc) >= 1 AND EXTRACT(DOW FROM start_utc) <= 5) AS total_weekday,
+count(visit_id) FILTER (WHERE EXTRACT(DOW FROM start_utc) = 0 OR EXTRACT(DOW FROM start_utc) = 6) AS total_weekend,
+(count(visit_id) FILTER (WHERE EXTRACT(DOW FROM start_utc) >= 1 AND EXTRACT(DOW FROM start_utc) <= 5)) / 5 AS avg_weekday,
+(count(visit_id) FILTER (WHERE EXTRACT(DOW FROM start_utc) = 0 OR EXTRACT(DOW FROM start_utc) = 6)) / 2 AS avg_weekend
+FROM visit 
+WHERE start_utc >= '2018-08-23 00:00:00' 
+AND finish_utc <= '2018-11-23 23:59:59';
+
+
+-- 9 [visit]
+-- Среднее количество гостей в утреннюю, дневную, вечернюю смену за октябрь и ноябрь
+-- Для этого не обязательно JOIN'ить таблицу shift, так как и там и там используется timestamp 
+-- и время смен всегда одигаковое: 08:00 - 15:00, 15:00 - 22:00, 22:00 - 08:00
+-- в базе время стоит в UTC, а Москва UTC+3. Поэтому в запросе пересменки в 05:00, 12:00 и 19:00
+SELECT
+place_id AS cafe_id,
+(count(visit_id) FILTER (WHERE start_utc::time >= '05:00:00'::time AND start_utc::time < '12:00:00'::time)) / 31 AS morning_shift,
+(count(visit_id) FILTER (WHERE start_utc::time >= '12:00:00'::time AND start_utc::time < '19:00:00'::time)) / 31 AS daytime_shift,
+(count(visit_id) FILTER (WHERE (start_utc::time >= '19:00:00'::time AND start_utc::time <= '23:59:59'::time) OR (start_utc::time >= '00:00:00'::time AND start_utc::time < '05:00:00'::time))) / 31 AS night_shift
+FROM visit 
+WHERE start_utc >= '2018-10-23 00:00:00'
+AND finish_utc <= '2018-11-23 23:59:59'
+GROUP BY place_id;
+
+
+
 
 
 
